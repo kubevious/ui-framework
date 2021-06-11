431a7ea40d733cc26b7d2bfa093bf45e8a3d68ab
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-06-10 17:01:36 -0700

    exposed http client retry.

--- src/backend-client.ts
+++ src/backend-client.ts
@@ -1,29 +1,34 @@
-import { HttpClient } from '@kubevious/http-client'
+import { Resolvable } from 'the-promise';
+import { HttpClient, RequestInfo } from '@kubevious/http-client'
 import { AuthorizerCb, HttpClientRetryOptions } from '@kubevious/http-client/dist/http-client';
 
 import { RemoteTrack } from './remote-track';
-
+import { request } from 'http';
 export interface BackendClientOptions
 {
-    timeout?: number,
-    retry?: HttpClientRetryOptions,
     headers?: Record<string, string>,
     authorizerCb?: AuthorizerCb,
+    canRetryCb?: (statusCode: any, reason: any, requestInfo: RequestInfo) => boolean,
 }
 
 export class BackendClient {
     private _client : HttpClient;
+    private _options : BackendClientOptions;
 
     constructor(urlBase?: string, remoteTrack?: RemoteTrack, options?: BackendClientOptions)
     {
-        options = options || {};
+        const retry : HttpClientRetryOptions = {
+            canContinueCb: this._canContinueCb.bind(this)
+        };
+
+        this._options = options || {};
+
         this._client = new HttpClient(urlBase || '', {
-            timeout: options.timeout,
             absorbFailures: true,
-            retry: options.retry,
-            headers: options.headers,
+            retry: retry,
+            headers: this._options.headers,
             tracker: remoteTrack,
-            authorizerCb: options.authorizerCb
+            authorizerCb: this._options.authorizerCb
         })
     }
 
@@ -31,4 +36,22 @@ export class BackendClient {
     {
         return this._client.scope(url || '');
     }
+
+    private _canContinueCb(reason: any, requestInfo: RequestInfo) : Resolvable<boolean>
+    {
+        const statusCode = reason.status || reason.statusCode || reason.code || reason.status_code;
+        if (statusCode == 400) {
+            return false;
+        } 
+
+        if (this._options.canRetryCb) {
+            if (!this._options.canRetryCb!(statusCode, reason, requestInfo)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+
 }
