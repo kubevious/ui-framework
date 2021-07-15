efed8582ae57d9ee70a49f56b2a0feef5ea1d73e
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-07-15 14:51:04 -0700

    fixes to global scope

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,31 @@
 import { Application } from './app' ;
 
-export const app : Application = new Application();
+function getApplication() : Application
+{
+    if (typeof window !== 'undefined') {
+        if ((<any>window).kubeviousApp) {
+            return (<any>window).kubeviousApp;
+        } else {
+            const app = new Application();
+            (<any>window).kubeviousApp = app;
+            return app;
+        }
+    }
+    
+    if (typeof global !== 'undefined') {
+        if ((<any>global).kubeviousApp) {
+            return (<any>global).kubeviousApp;
+        } else {
+            const app = new Application();
+            (<any>global).kubeviousApp = app;
+            return app;
+        }
+    }
+
+    throw new Error("global or window not found.");
+}
+
+export const app : Application = getApplication();
 
 export const sharedState = app.sharedState;
 export const operationLog = app.operationLog;
--- test/global-app.ts
+++ test/global-app.ts
@@ -7,6 +7,10 @@ describe('global-app', () => {
 
     it('test-01', () => {
         should(app).be.ok();
+        
+        app.initHttpClient()
+        let client = app.httpClient('/url/v1/test')
+        should(client).be.ok();
     });
 
 });
